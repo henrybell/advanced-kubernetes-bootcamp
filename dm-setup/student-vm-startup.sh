5116d7de59986f7685fceb7c39fbe6658779edb2
Ameer Abbas <ameer00@gmail.com>
2018-06-25 12:09:27 -0700

    Update student-vm-startup.sh

--- dm-setup/student-vm-startup.sh
+++ dm-setup/student-vm-startup.sh
@@ -190,5 +190,5 @@ accounts:
   password: '${SA_JSON}'
   email: 1234@5678.com
 EOF
-    helm install -n adv-k8s stable/spinnaker -f spinnaker-config.yaml --timeout 600
+    helm install -n adv-k8s stable/spinnaker -f spinnaker-config.yaml --timeout 600 --version 0.5.0
 done
